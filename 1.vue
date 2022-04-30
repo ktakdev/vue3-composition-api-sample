c07268bfb79ec33daa1017c791bff9a5570ff6c9
ktak <102935983+ktakdev@users.noreply.github.com>
2022-04-30 18:58:14 +0900

    Create 1.vue

---
+++ 1.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+export interface TextFormProps {
+  value?: string;
+}
+
+defineProps<TextFormProps>()
+</script>
+
+<template>
+  <div class="text-form">
+    <input type="text" :value="value" />
+  </div>
+</template>
